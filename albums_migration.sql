104a2ff747c94e4778c96677618214939d84d2d7
HarpersGeekly <HarperRyanC@gmail.com>
2017-10-10 12:07:31 -0500

    Initial commit. Created table albums_migration and gitignore.

---
+++ albums_migration.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS redwood_test_db;
+USE redwood_test_db;
+DROP TABLE IF EXISTS albums;
+
+-- Create an albums table with the following columns:
+CREATE TABLE IF NOT EXISTS albums (
+-- id — auto incrementing unsigned integer primary key
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+-- artist — string for storing the recording artist name
+artist VARCHAR(200) NOT NULL,
+-- name — string for storing a record name
+name VARCHAR(300) NOT NULL,
+-- release_date — integer representing year record was released
+release_date DATE NOT NULL,
+-- sales — floating point value for number of records sold (in millions)
+sales DECIMAL(10, 2) NOT NULL,
+-- genre — string for storing the record's genre(s)
+genre VARCHAR(50) NOT NULL,
+
+PRIMARY KEY (id)
+);
+-- # not null means required
