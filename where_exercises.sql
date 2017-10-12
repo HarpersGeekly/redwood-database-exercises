578e33ffa78cb71e138916f991ae3cc33702f460
HarpersGeekly <HarperRyanC@gmail.com>
2017-10-12 12:10:02 -0500

    Correction: SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT '%qu%'; To: SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';

--- where_exercises.sql
+++ where_exercises.sql
@@ -24,4 +24,4 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 -- Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25%';
 -- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT '%qu%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
