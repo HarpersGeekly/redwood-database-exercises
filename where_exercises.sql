e42f96967cd9fda192c9112f0614039b4e1cf191
HarpersGeekly <HarperRyanC@gmail.com>
2017-10-12 10:49:43 -0500

    Using newly imported employees database, exercise using WHERE, LIKE, IN, AND, AND NOT, OR

---
+++ where_exercises.sql
@@ -0,0 +1,27 @@
+-- Create a file named where_exercises.sql. Make sure to use
+-- the employees database
+USE employees;
+-- Write individual queries to find the following things:
+-- Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+-- Employees whose last name starts with 'E' — 7,330 rows.
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+-- Employees hired in the 90s — 135,214 rows.
+SELECT * FROM employees WHERE hire_date LIKE '199%';
+-- Employees born on Christmas — 842 rows.
+SELECT * FROM employees where birth_date LIKE '%-12-25%';
+-- Employees with a 'q' in their last name — 1,873 rows.
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+-- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+-- Now add a condition to find everybody with those names who is also male — 441 rows.
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' AND gender - 'M';
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+-- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+-- Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25%';
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT '%qu%';
